2c81b47f1f70fb30342846d5ced101dd83b33512
Denise Chang <ddleletan@hotmail.com>
2024-01-29 17:33:20 -0500

    visualize data

--- week_4/quizz_4/01_visualize_data.R
+++ week_4/quizz_4/01_visualize_data.R
@@ -8,4 +8,25 @@
 #### Workspace setup ####
 library(tidyverse)
 
+#### Read in data ####
+sim_data <- 
+  read_csv(file = "week_4/quizz_4/sim_data.csv")
+
 #### Visualize data ####
+# Reshape the tibble
+long_sim_data <- 
+  pivot_longer(sim_data, 
+               cols = c("edward", "hugo", "lucy"), 
+               names_to = "person", 
+               values_to = "height")
+
+# Construct graph
+ggplot(long_sim_data, aes(x = as.factor(friend),
+                     y = height,
+                     colour = person)) +
+  geom_point() +
+  geom_line() +
+  labs(title = "Height Measurements",
+       x = "Individual Measured",
+       y = "Height (cm)") +
+  theme_minimal()
