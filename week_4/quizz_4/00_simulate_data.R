3dbacfd5461e3d6dbbf69ce05c9a728517233d2c
Denise Chang <ddleletan@hotmail.com>
2024-01-29 16:54:59 -0500

    simulate data

--- week_4/quizz_4/00_simulate_data.R
+++ week_4/quizz_4/00_simulate_data.R
@@ -13,15 +13,16 @@ library(tidyverse)
 set.seed(302) # random seed
 
 # errors in measurements
-hugo_error <- 3
+hugo_error <- 4
+edward_error <- 4
 lucy_error <- 2
 random_error <- 1
 
 sim_data <-
   tibble(
     friend = 1:20,
-    # simulates Edward's measurements
-    edward = sample(x = 161:180,
+    # simulates real measurements
+    real = sample(x = 161:180,
                     size = 20,
                     replace = TRUE,
                     prob = c(0.05, 0.05, 0.05, 0.05, 0.05,
@@ -29,17 +30,26 @@ sim_data <-
                              0.05, 0.05, 0.05, 0.05, 0.05,
                              0.05, 0.05, 0.05, 0.05, 0.05))) |>
   mutate(
+    # simulates Edward's measurements
+    edward = real + sample(x = c(edward_error, random_error, 0),
+                           size = 20, 
+                           replace = TRUE, 
+                           prob = c(0.3, 0.4, 0.3)),
     # simulates Hugo's measurements
-    hugo = edward + sample(x = c(hugo_error, random_error, 0),
-                                size = 20,
-                                replace = TRUE,
-                                prob = c(0.4, 0.5, 0.1)),
+    hugo = real + sample(x = c(hugo_error, random_error, 0),
+                         size = 20,
+                         replace = TRUE,
+                         prob = c(0.4, 0.5, 0.1)),
     # simulates Lucy's measurements
-    lucy = edward + sample(x = c(lucy_error, random_error, 0),
-                                size = 20,
-                                replace = TRUE, 
-                                prob = c(0.6, 0.3, 0.1))
+    lucy = real + sample(x = c(lucy_error, random_error, 0),
+                         size = 20,
+                         replace = TRUE, 
+                         prob = c(0.6, 0.3, 0.1))
   )
+
+sim_data <-
+  sim_data |>
+  select(friend, edward, hugo, lucy)
 
 #### Test Simulation ####
 
@@ -49,8 +59,8 @@ sim_data$hugo |> length() == 20
 sim_data$lucy |> length() == 20
 
 # check Edward's measurement range
-sim_data$edward |> min() >= 161
-sim_data$edward |> max() <= 180
+sim_data$edward |> min() >= 161 - edward_error
+sim_data$edward |> max() <= 180 + edward_error
 
 # check Hugo's measurement range
 sim_data$hugo |> min() >= 161 - hugo_error 
