a9617d38013e276df463fd361aba9c11169f8751
Denise Chang <ddleletan@hotmail.com>
2024-02-02 21:45:41 -0500

    stimulate data

---
+++ week_5/mini_quiz_5/00_simulate_data.R
@@ -0,0 +1,29 @@
+#### Preamble ####
+# Purpose: Simulate data on Canadian Prime Ministers
+# Author: Denise Chang
+# Date: 14 January 2024 
+# Prerequisite: None
+
+
+#### Workspace setup ####
+library(tidyverse)
+library(rvest)
+library(babynames)
+
+#### Start of simulation ####
+
+set.seed(302) #random seed
+
+sim_data <-
+  tibble(
+    prime_minister = babynames |>
+      filter(prop > 0.01) |>
+      distinct(name) |>
+      unlist() |>
+      sample(size = 10, replace = FALSE),
+    birth_year = sample(1700:1990, size = 10, replace = TRUE),
+    years_lived = sample(50:100, size = 10, replace = TRUE),
+    death_year = birth_year + years_lived
+  ) |>
+  select(prime_minister, birth_year, death_year, years_lived) |>
+  arrange(birth_year)
