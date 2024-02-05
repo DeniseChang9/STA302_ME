1ea9b56c0eef04522cee20da06ad1ea1f5620368
DeniseChang9 <156145228+DeniseChang9@users.noreply.github.com>
2024-02-05 16:49:38 -0500

    Update 01_visualize_data.R

--- week_5/quiz_5/01_visualize_data.R
+++ week_5/quiz_5/01_visualize_data.R
@@ -2,7 +2,7 @@
 # Purpose: Visualize Data on pages read by five friends
 # Author: Denise Chang
 # Email: ddleletan@hotmail.com
-# Date: 29 January 2024 
+# Date: 5 February 2024 
 # Prerequisite: 00_simulate_data.R
 
 #### Workspace setup ####
