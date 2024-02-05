18d1f172adebbc1d8d380b5ede20f234f318e361
Denise Chang <ddleletan@hotmail.com>
2024-02-05 16:45:52 -0500

    visualize simulates data

--- week_5/quiz_5/01_visualize_data.R
+++ week_5/quiz_5/01_visualize_data.R
@@ -8,3 +8,26 @@
 #### Workspace setup ####
 library(tidyverse)
 
+#### Read in data ####
+sim_data <- 
+  read_csv(file = "week_5/quiz_5/sim_data.csv")
+
+#### Visualize data ####
+# Reshape the tibble
+long_sim_data <- 
+  pivot_longer(sim_data, 
+               cols = c("Matt", "Ash","Jacki", "Rol", "Mike"), 
+               names_to = "person", 
+               values_to = "pages")
+
+# Construct graph
+ggplot(long_sim_data, aes(x = as.factor(day),
+                          y = pages,
+                          colour = person)) +
+  geom_point() +
+  geom_line() +
+  labs(title = "Daily pages read by friend",
+       x = "Day",
+       y = "Number of pages read") +
+  theme_minimal() + 
+  scale_x_discrete(breaks = c("20", "40", "60", "80", "100"))
