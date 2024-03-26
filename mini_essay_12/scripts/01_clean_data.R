8f1107a58cf6cdd11d72ab7e9581f27d86d59617
Denise Chang <ddleletan@hotmail.com>
2024-03-26 12:30:11 -0400

    download data

---
+++ mini_essay_12/scripts/01_clean_data.R
@@ -0,0 +1,8 @@
+#### Pre-amble ####
+# Clean data from : https://www.ushmm.org/online/hsv/source_view.php?SourceId=49478
+
+
+# Workspace set up
+library(tidyverse)
+
+raw_data <- read_csv("input/raw_data.csv")
